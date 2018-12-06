4a05c2bc975379b3835220f012d94b35eb188f78
anners <anners@google.com>
2018-12-06 09:26:13 -0800

    removed peering with the mgmt vpc

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -13,26 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-resource "google_compute_network_peering" "pci_mgmt_peering" {
+resource "google_compute_network_peering" "pci_nonpci_peering" {
   name         = "pcipeering"
   network      = "${google_compute_network.pci_shared_network.self_link}"
-  peer_network = "${google_compute_network.mgmt_shared_network.self_link}"
+  peer_network = "${google_compute_network.nonpci.self_link}"
 }
 
-resource "google_compute_network_peering" "nonpci_mgmt_peering" {
+resource "google_compute_network_peering" "nonpci_pci_peering" {
   name         = "nonpcipeering"
   network      = "${google_compute_network.nonpci_shared_network.self_link}"
-  peer_network = "${google_compute_network.mgmt_shared_network.self_link}"
-}
-
-resource "google_compute_network_peering" "mgmt_pci_peering" {
-  name         = "mgmtpcipeering"
-  network      = "${google_compute_network.mgmt_shared_network.self_link}"
-  peer_network = "${google_compute_network.pci_shared_network.self_link}"
-}
-
-resource "google_compute_network_peering" "mgmt_nonpci_peering" {
-  name         = "mgmtnonpcipeering"
-  network      = "${google_compute_network.mgmt_shared_network.self_link}"
-  peer_network = "${google_compute_network.nonpci_shared_network.self_link}"
+  peer_network = "${google_compute_network.pci.self_link}"
 }
