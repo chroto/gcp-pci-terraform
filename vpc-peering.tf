a832b0395f05d8fbb3125ebf6946701f65195d6a
anners <anners@google.com>
2018-12-06 18:13:26 -0800

    updates to peering and sql

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -16,11 +16,11 @@
 resource "google_compute_network_peering" "pci_nonpci_peering" {
   name         = "pcipeering"
   network      = "${google_compute_network.pci_shared_network.self_link}"
-  peer_network = "${google_compute_network.nonpci.self_link}"
+  peer_network = "${google_compute_network.nonpci_shared_network.self_link}"
 }
 
 resource "google_compute_network_peering" "nonpci_pci_peering" {
   name         = "nonpcipeering"
   network      = "${google_compute_network.nonpci_shared_network.self_link}"
-  peer_network = "${google_compute_network.pci.self_link}"
+  peer_network = "${google_compute_network.pci_shared_network.self_link}"
 }
